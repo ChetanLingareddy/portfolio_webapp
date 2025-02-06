048b40e8e863f6b5740cb7b086b2593debd5f489
ChetanLingareddy <lingareddychetan@gmail.com>
2025-02-06 00:34:34 -0600

    Added all the project images and titles.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas
 
 st.set_page_config(layout= "wide")
 
@@ -16,4 +17,22 @@ with col2:
 content2= """
 below are some web apps i have created using python , please feel free to contact me!
 """
-st.write(content2)
+st.caption(content2)
+
+col3,empty_col, col4 = st.columns([1.5,0.5,1.5])
+
+df= pandas.read_csv("data.csv", sep = ";")
+with col3:
+    for index,  row in df[:10].iterrows():
+        st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source Code]({"https://github.com/ChetanLingareddy/Todo_public"})")
+
+
+with col4:
+    for index,  row in df[10:].iterrows():
+        st.header(row["title"])
+        st.write ( row["description"] )
+        st.image ( "images/" + row["image"] )
+        st.write(f"[Source Code]({row['url']})")
